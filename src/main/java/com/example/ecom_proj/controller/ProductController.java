73c18da3d8bc21290d0e07640d5c01693c4f451d
Rohan Marya <rohanmarya1507@gmail.com>
2026-06-14 14:31:38 +0530

    response entity and getProductById added

--- src/main/java/com/example/ecom_proj/controller/ProductController.java
+++ src/main/java/com/example/ecom_proj/controller/ProductController.java
@@ -3,9 +3,9 @@ package com.example.ecom_proj.controller;
 import com.example.ecom_proj.model.Product;
 import com.example.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +22,16 @@ public class ProductController {
         return "Hello World!";
     }
 
-    @RequestMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+        Product p = service.getProductById(id);
+        HttpStatus s = p == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(p,s);
     }
 
 }
